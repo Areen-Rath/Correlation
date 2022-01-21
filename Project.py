2ab00111425561462f8cda05f6e0c3ebfcc241b6
Areen-Rath <70901420+Areen-Rath@users.noreply.github.com>
2022-01-21 16:53:48 +0530

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,34 @@
+import csv
+import numpy as np
+import plotly.express as px
+
+def get_data_source(data_path):
+    roll_no = []
+    marks = []
+
+    with open(data_path, newline='') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            roll_no.append(int(float(row["Roll No"])))
+            marks.append(int(float(row["Marks"])))
+
+    return {
+        "x": roll_no,
+        "y": marks
+    }
+
+def find_correlation(data_source):
+    correlation = np.corrcoef(data_source["x"], data_source["y"])
+    print(f"Correlation is {correlation[0, 1]}")
+
+def plot_figure(data_source):
+    fig = px.scatter(x = data_source["x"], y = data_source["y"])
+    fig.show()
+
+def setup():
+    data_path = "Student Marks vs Days Present.csv"
+    data_source = get_data_source(data_path)
+    find_correlation(data_source)
+    plot_figure(data_source)
+
+setup()
